253baf8cdb1b1588c70858b3ce13ac45ad5e5a7d
kiki1373639299 <zkai0106@163.com>
2026-02-04 19:10:26 +0800

    constants: 新增全局常量定义
    - globals.ts: 全局常量配置
    - index.ts: 常量导出入口

---
+++ src/constants/globals.ts
@@ -0,0 +1,16 @@
+/** layout content 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_CONTENT_HEIGHT = `--continew-content-height`;
+/** layout content 组件的宽度 */
+export const CSS_VARIABLE_LAYOUT_CONTENT_WIDTH = `--continew-content-width`;
+/** layout header 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_HEADER_HEIGHT = `--continew-header-height`;
+/** layout footer 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_FOOTER_HEIGHT = `--continew-footer-height`;
+
+/** 内容区域的组件ID */
+export const ELEMENT_ID_MAIN_CONTENT = `__continew_main_content`;
+
+/**
+ * @zh_CN 默认命名空间
+ */
+export const DEFAULT_NAMESPACE = 'continew';
