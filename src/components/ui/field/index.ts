28c25ea49d5f9badd137040ffe4d66b723230ed6
ppxb <317842449@qq.com>
2026-02-05 12:50:30 +0800

    chore: update field group style

--- src/components/ui/field/index.ts
+++ src/components/ui/field/index.ts
@@ -2,19 +2,19 @@ import type { VariantProps } from 'class-variance-authority';
 import { cva } from 'class-variance-authority';
 
 export const fieldVariants = cva(
-  'group/field flex w-full gap-3 data-[invalid=true]:text-destructive',
+  'group/field flex w-full gap-2 data-[invalid=true]:text-destructive',
   {
     variants: {
       orientation: {
-        vertical: ['flex-col [&>*]:w-full [&>.sr-only]:w-auto'],
+        vertical: ['flex-col *:w-full [&>.sr-only]:w-auto'],
         horizontal: [
           'flex-row items-center',
-          '[&>[data-slot=field-label]]:flex-auto',
+          '*:data-[slot=field-label]:flex-auto',
           'has-[>[data-slot=field-content]]:items-start has-[>[data-slot=field-content]]:[&>[role=checkbox],[role=radio]]:mt-px',
         ],
         responsive: [
-          'flex-col [&>*]:w-full [&>.sr-only]:w-auto @md/field-group:flex-row @md/field-group:items-center @md/field-group:[&>*]:w-auto',
-          '@md/field-group:[&>[data-slot=field-label]]:flex-auto',
+          'flex-col *:w-full [&>.sr-only]:w-auto @md/field-group:flex-row @md/field-group:items-center @md/field-group:*:w-auto',
+          '@md/field-group:*:data-[slot=field-label]:flex-auto',
           '@md/field-group:has-[>[data-slot=field-content]]:items-start @md/field-group:has-[>[data-slot=field-content]]:[&>[role=checkbox],[role=radio]]:mt-px',
         ],
       },
