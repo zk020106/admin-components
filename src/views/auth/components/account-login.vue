9a56503c56fafd08cb686118c56553d28bf26d24
ppxb <317842449@qq.com>
2026-02-05 12:51:59 +0800

    chore: add login validation

--- src/views/auth/components/account-login.vue
+++ src/views/auth/components/account-login.vue
@@ -1,23 +1,38 @@
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { toTypedSchema } from '@vee-validate/zod';
+import { useForm, Field as VeeField } from 'vee-validate';
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
+import * as z from 'zod';
 import { fetchImgCaptcha } from '@/apis/captcha';
 import { Button } from '@/components/ui/button';
-import { Field, FieldDescription, FieldGroup } from '@/components/ui/field';
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+} from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import { $t } from '@/locales';
 
-interface AccountLoginForm {
-  tenantId: string;
-  username: string;
-  password: string;
-  captcha: string;
-}
+const formSchema = computed(() =>
+  toTypedSchema(
+    z.object({
+      tenantId: z.string().optional(),
+      username: z.string().min(1, $t('auth.account.form.username.validator')),
+      password: z.string().min(1, $t('auth.account.form.password.validator')),
+      captcha: z.string().length(4, $t('auth.account.form.captcha.validator')),
+    }),
+  ),
+);
 
-const form = ref<AccountLoginForm>({
-  tenantId: '',
-  username: 'admin',
-  password: 'admin123',
-  captcha: '',
+const { handleSubmit } = useForm({
+  validationSchema: formSchema,
+  initialValues: {
+    tenantId: '',
+    username: 'admin',
+    password: 'admin123',
+    captcha: '',
+  },
 });
 
 const captchaUUID = ref('');
@@ -27,10 +42,10 @@ const isCaptchaExpired = ref(false);
 
 let timer: ReturnType<typeof setTimeout> | null = null;
 
-function handleLogin() {
+const onSubmit = handleSubmit((values) => {
   // eslint-disable-next-line no-console
-  console.log('Logging in with', form.value.username, form.value.password);
-}
+  console.log('Logging in with ', values);
+});
 
 async function getImgCaptcha() {
   const { data, error } = await fetchImgCaptcha();
@@ -38,7 +53,6 @@ async function getImgCaptcha() {
     captchaUUID.value = data.uuid;
     captchaImg.value = data.img;
     captchaExpireTime.value = data.expireTime;
-    form.value.captcha = '';
     isCaptchaExpired.value = false;
 
     startExpireTimer(data.expireTime);
@@ -70,7 +84,7 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <form class="p-6 md:p-8" @submit.prevent="handleLogin">
+  <form id="login-form" class="p-6 md:p-8" @submit="onSubmit">
     <FieldGroup>
       <div class="flex flex-col gap-2">
         <h1 class="text-2xl font-bold">
@@ -81,71 +95,87 @@ onBeforeUnmount(() => {
         </p>
       </div>
 
-      <Field>
-        <Input
-          v-model="form.tenantId"
-          :placeholder="$t('auth.account.form.tenant.placeholder')"
-          required
-        />
-      </Field>
-
-      <Field>
-        <Input
-          v-model="form.username"
-          :placeholder="$t('auth.account.form.username.placeholder')"
-          required
-        />
-      </Field>
-
-      <Field>
-        <Input
-          v-model="form.password"
-          type="password"
-          :placeholder="$t('auth.account.form.password.placeholder')"
-          required
-        />
-        <div class="flex items-center">
-          <a
-            href="#"
-            class="ml-auto text-sm underline-offset-2 hover:underline"
-          >
-            {{ $t('auth.account.forgetPwd') }}
-          </a>
-        </div>
-      </Field>
-
-      <Field>
-        <div class="flex gap-2">
+      <VeeField v-slot="{ field, errors }" name="tenantId">
+        <Field :data-invalid="!!errors.length">
           <Input
-            id="captcha"
-            v-model="form.captcha"
-            :placeholder="$t('auth.account.form.captcha.placeholder')"
-            class="flex-1"
-            required
+            v-bind="field"
+            :placeholder="$t('auth.account.form.tenant.placeholder')"
+            autocomplete="off"
+            :aria-invalid="!!errors.length"
           />
+          <FieldError v-if="errors.length" :errors="errors" />
+        </Field>
+      </VeeField>
 
-          <div class="relative w-22 shrink-0 md:w-28" @click="getImgCaptcha">
-            <img
-              v-if="captchaImg"
-              :src="captchaImg"
-              alt="Captcha"
-              class="size-full rounded-lg object-contain hover:cursor-pointer"
+      <VeeField v-slot="{ field, errors }" name="username">
+        <Field :data-invalid="!!errors.length">
+          <Input
+            :model-value="field.value"
+            :placeholder="$t('auth.account.form.username.placeholder')"
+            autocomplete="username"
+            :aria-invalid="!!errors.length"
+            @update:model-value="field.onChange"
+          />
+          <FieldError v-if="errors.length" :errors="errors" />
+        </Field>
+      </VeeField>
+
+      <VeeField v-slot="{ field, errors }" name="password">
+        <Field :data-invalid="!!errors.length">
+          <Input
+            :model-value="field.value"
+            type="password"
+            :placeholder="$t('auth.account.form.password.placeholder')"
+            autocomplete="current-password"
+            :aria-invalid="!!errors.length"
+            @update:model-value="field.onChange"
+          />
+          <FieldError v-if="errors.length" :errors="errors" />
+          <FieldDescription class="text-right">
+            <div
+              class="hover:text-primary text-sm no-underline underline-offset-2 hover:cursor-pointer hover:underline"
+            >
+              {{ $t('auth.account.forgetPwd') }}
+            </div>
+          </FieldDescription>
+        </Field>
+      </VeeField>
+
+      <VeeField v-slot="{ field, errors }" name="captcha">
+        <Field :data-invalid="!!errors.length">
+          <div class="flex gap-2">
+            <Input
+              v-bind="field"
+              :placeholder="$t('auth.account.form.captcha.placeholder')"
+              class="flex-1"
+              autocomplete="off"
+              :aria-invalid="!!errors.length"
             />
 
-            <div
-              v-if="isCaptchaExpired"
-              class="absolute inset-0 flex items-center justify-center rounded-lg bg-black/60 backdrop-blur hover:cursor-pointer"
-            >
-              <span class="text-xs text-white">
-                {{ $t('auth.account.form.captcha.expired') }}
-              </span>
+            <div class="relative w-22 shrink-0 md:w-28" @click="getImgCaptcha">
+              <img
+                v-if="captchaImg"
+                :src="captchaImg"
+                alt="Captcha"
+                class="size-full rounded-lg object-contain hover:cursor-pointer"
+              />
+
+              <div
+                v-if="isCaptchaExpired"
+                class="absolute inset-0 flex items-center justify-center rounded-lg bg-black/60 backdrop-blur hover:cursor-pointer"
+              >
+                <span class="text-xs text-white">
+                  {{ $t('auth.account.form.captcha.expired') }}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      </Field>
+          <FieldError v-if="errors.length" :errors="errors" />
+        </Field>
+      </VeeField>
 
       <Field>
-        <Button type="submit" @click="handleLogin">
+        <Button type="submit" form="login-form">
           {{ $t('auth.account.submit') }}
         </Button>
       </Field>
